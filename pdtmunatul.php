6ab2c777e77a0ebfc4cf446479d69a46654acf5f
SFourUn <95516577+SFourUn@users.noreply.github.com>
2023-09-01 22:55:36 +0700

    Add files via upload

---
+++ pdtmunatul.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biodata Saya</title>
+</head>
+<body>
+    <h1>Biodata Saya</h1>
+    <p>Nama: Munatul Awwaliyah</p>
+    <p>Usia: 20 tahun</p>
+    <p>Alamat: Tasikmalaya</p>
+    <p>Email: munatulawwaliyah@gmail.com</p>
+    <p>No. Telepon: 082121750340</p>
+    
+    <?php
+        // Variabel PHP
+        $hobi = "Mendengar Musik";
+        $pendidikan = "S1 Informatika";
+
+        // Menampilkan informasi tambahan
+        echo "<p>Hobi: $hobi</p>";
+        echo "<p>Pendidikan: $pendidikan</p>";
+    ?>
+</body>
+</html>
