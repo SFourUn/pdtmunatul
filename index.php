39383f829ca9352a48d5e7eda0ee27c07c397082
SFourUn <95516577+SFourUn@users.noreply.github.com>
2023-09-02 02:38:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/c83b42e88c.js" crossorigin="anonymous"></script>
+    <link href="bootstrap/Font-Awesome/css/fontawesome.min.css" type="text/css" rel="stylesheet">
+    <title>Zakat Fitrah</title>
+  </head>
+  <div class="dashbord">
+  <body>
+  <!-- Side Bar -->
+   <div class="main-container d-flex">
+    <div class="sidebar" id="side_nav">
+      <div class="header-box px-2 pt-3 pb-4">
+        <h1 class="fs-4"><span class="bg-black text-dark rounded shadow px-2 me-2"></span><span class="text-white"><i class="fa-solid fa-mosque"></i> Zakat Fitrah</span></h1>
+        <button class="btn d-md-none d-block close-btn px-1 py-0 text-white"></button>
+      </div>
+        <ul class="list-unstyled px-2">
+          <li class=""><a href="index.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-house"></i> Home</a></li>
+        </ul>
+        <hr class="h-color mx-3">
+        <ul class="list-unstyled px-2">
+          <li class=""><a href="muzakki.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-user"></i> Muzakki</a></li>
+          <li class=""><a href="mustahik.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-users"></i> Mustahik</a></li>
+          <li class=""><a href="pembayaran.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-hand-holding-dollar"></i> Pembayaran Zakat</a></li>
+          <li class=""><a href="distribusiwarga.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-truck-fast"></i> Distribusi Zakat Warga</a></li>
+          <li class=""><a href="distribusimustahik.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-truck-front"></i> Distribusi Zakat Mustahik</a></li>
+        </ul>
+        <hr class="h-color mx-3">
+        <ul class="list-unstyled px-2">
+          <li class=""><a href="pengumpulan.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-clipboard"></i> Pengumpulan Zakat</a></li>
+          <li class=""><a href="laporan.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-file"></i> Laporan Distribusi Zakat</a></li>
+        </ul>
+      <div class="dropdown open p-5">
+        <button type="button" class="btn btn-secondary text-white" id="triggerId" aria-expanded="false">
+        <span class="ms-1 me-1"> <i class="fa-solid fa-arrow-right-to-bracket"></i> Logout </span>
+        </button>
+      </div>
+    </div>  
+  <!-- End Of Side Bar -->
+    <div class="content">
+<!-- Navigation -->
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand"><h5>Selamat Datang!</h5></a>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success btn-bg-black" type="submit">Search</button>
+          </form>
+      </div>
+    </nav>
+<!-- Enf Of Navigation -->
+<br>
+<br>
+<br>
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Muzakki</h5>
+        <p class="card-text">Muzakki adalah orang yang dikenai kewajiban membayar zakat atas kepemilikan harta yang telah mencapai nishab dan haul.</p>
+        <a href="muzakki.php" class="btn btn-primary">Lihat Detail</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="btn btn-primary">Lihat Detail</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Lihat Detail</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="btn btn-primary">Lihat Detail</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+    </div>
+   </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    
+  </body>
+  </div>
+</html>
